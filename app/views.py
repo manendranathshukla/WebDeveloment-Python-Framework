5b58bf7fd2926f9d4544eb6ff4ac1e2db248b0cc
Manendra Nath Shukla <51334809+manendranathshukla@users.noreply.github.com>
2019-08-22 22:28:23 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,57 +1,127 @@
-from flask import render_template,request,send_file,redirect,make_response
+from flask import render_template,request,send_file,redirect,make_response,url_for
 from app import app
 import os
 import io
 import csv
 import pandas as pd
+import numpy as np
+
+#for saving the uploaded to uploads folder
 
 app.config["DATA_UPLOADS"] = "app/static/data/uploads"
 
 
-
+#main page
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+#for receving the data from homepage
 
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def get_vtu_no():
+    select = request.form.get('comp_select')
+    option=str(select)
+    date=request.form['examdate']
     vtuno=request.form['text']
-    text = open('app/static/data/uploads/seatplanning.csv','r+')
-    #content = text.read()
-    #text.close()
-    #procesed=("Dear Student %s,You don't have any exam today,go attend ur classes.\n Thank You!!!"%vtuno)
-    content=find(vtuno,text)
-    return render_template("index.html",content=content)
+    l=list(date.split('-'))
+    date1=str(l[2])+"-"
+    date1+=str(l[1])+"-"
+    date1+=str(l[0])
+    if(option=="CSE"):
+        text = open('app/static/data/uploads/seatplanningCSE.csv','r')
+        content=find(vtuno,text,date1)
+    if(option=="CIVIL"):
+        text = open('app/static/data/uploads/seatplanningCIVIL.csv','r')
+        content=find(vtuno,text,date1)
+
+    if(option=="ECE"):
+        text = open('app/static/data/uploads/seatplanningECE.csv','r')
+        content=find(vtuno,text,date1)
+    if(option=="MECH"):
+        text = open('app/static/data/uploads/seatplanningMECH.csv','r')
+        content=find(vtuno,text,date1)
+    if(option=="AERO"):
+        text = open('app/static/data/uploads/seatplanningAERO.csv','r')
+        content=find(vtuno,text,date1)
+    if(option=="EEE"):
+        text = open('app/static/data/uploads/seatplanningEEE.csv','r')
+        content=find(vtuno,text,date1)
+    if(option=="IT"):
+        text = open('app/static/data/uploads/seatplanningIT.csv','r')
+        content=find(vtuno,text,date1)
+
+    inputquery=""
+    inputquery+=str(date)
+    inputquery+=" "
+    inputquery+=str(vtuno)
+    #content=find(vtuno,text,date1)
+    newdate=""
+    newdate+="Exam Date: "+date
+    newOption="";newOption+="Department: "+option
+    return render_template("index.html",exa=newdate,newOption=newOption,content=content)
 
 
+#for seaching the data
 
-
-def find(vtuno,text):
+def find(vtuno,text,date1):
     lines=text.readlines()
-    result=""
+    room=name=vtu=date=cc=cn=seatno=""
     flag=0
+    t=" "
     for i in lines:
         dd=list(i.split(','))
-        if(dd[0]==vtuno):
-            result+=dd[1]
+        if(dd[1]==vtuno and dd[0]==date1):
+            date+=dd[0]+t
+            room+=dd[5]+t
+            name+=dd[2]+t
+            vtu+=dd[1]+t
+            cc+=dd[3]+t
+            cn+=dd[4]+t
+            seatno+=dd[6]+t
             flag=1
+            break
+
     if(flag==0):
-        return("Dear Student %s,You don't have any exam today,go attend ur classes.\n Thank You!!!"%(vtuno))
+        return("Data Not Found!!!")
     else:
-        return("You Have Exam in Room No:%s"%(result))
+        return(" VTU No: %s \n Student Name: %s \n Course Code: %s \n Course Name: %s \n Room No: %s \n Seat No: %s "%(vtu,name,cc,cn,room,seatno))
+
+
+
+
+#For login data page
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    #userdata = pd.read_csv('app/static/data/uploads/userdata.csv')
+    userdata=open('app/static/data/uploads/userdata.csv','r')
+    lines=userdata.readlines()
+
+    error=None
+    f=0
+    if(request.method=='POST'):
+        #for d in lines:
+        #    ll=list(d.split(','))
+        if((request.form['username'] != 'admin') or (request.form['password'] != 'pass')):
+                #f=1
+            error='Invalid Credential. Please try again.'
+
+        else:
+            return redirect(url_for('upload'))
+
+    return render_template('login.html',error=error)
 
 
 
 
 
 
+#for uploading the data file
 
-@app.route("/upload-data", methods=["GET", "POST"])
-def upload_data():
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
     if request.method == "POST":
 
         if request.files:
@@ -61,59 +131,10 @@ def upload_data():
 
             image.save(os.path.join(app.config["DATA_UPLOADS"], image.filename))
             return redirect(request.url)
-    return render_template("upload_data.html")
-#def importdata(file):
+    return render_template("upload.html")
 
 
 
-
-@app.route('/form')
-def form():
-    return """
-        <html>
-            <body>
-                <h1>Transform a file demo</h1>
-
-                <form action="/transform" method="post" enctype="multipart/form-data">
-                    <input type="file" name="data_file" />
-                    <input type="submit" />
-                </form>
-            </body>
-        </html>
-    """
-
-
-
-
-
-@app.route('/transform', methods=["POST"])
-def transform_view():
-    file = request.files['data_file']
-    if not file:
-        return "No file"
-
-    #stream = io.StringIO(f.stream.read().decode("UTF8"), newline=None)
-    #csv_input = csv.reader(stream)
-    #print("file contents: ", file_contents)
-    #print(type(file_contents))
-    #print(csv_input)
-    #dict={}
-    #for row in csv_input:
-    #    dict[row[0]]=row[1]
-    #THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
-    path="app/static/data/uploads"
-    filename_path = os.path.join(path, file.filename)
-
-    # Put input file in dataframe
-    sheet = pd.read_csv(filename_path, encoding='cp1252')
-    #stream.seek(0)
-    #result = transform(stream.read())
-    #sheet=pd.read_csv(f,encoding='cp1252')
-    #response = make_response(result)
-    #response.headers["Content-Disposition"] = "attachment; filename=result.csv"'''
-    #return (dict)
-    return(sheet)
-    #return None
 
 
 @app.route('/about')
@@ -123,4 +144,3 @@ def about():
     <p>I Love Python!!!!</p>
     <code>I am a <em>freelancer</em></code>
     """)
-
